14a25f492437f1ce3407854a0617a866169f7e4b
edveloso <edveloso@gmail.com>
2020-08-01 04:47:44 -0300

    mostrando o item selecionado na pagina de compra

--- lojasnet-web/src/main/java/br/infnet/vitrine/VitrineBean.java
+++ lojasnet-web/src/main/java/br/infnet/vitrine/VitrineBean.java
@@ -2,8 +2,10 @@ package br.infnet.vitrine;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import br.infnet.catalogo.ILivroService;
@@ -16,6 +18,8 @@ public class VitrineBean {
 	private ILivroService service;
 
 	private List<Livro> livros;
+	
+	private Integer quantidade;
 
 	private Livro livro;
 
@@ -23,6 +27,28 @@ public class VitrineBean {
 		livros = new ArrayList<Livro>();
 		livro = new Livro();
 	}
+	
+	public String adicionarAoCarrinho() {
+		
+		return "/vitrine/carrinho";
+	}
+	
+	public String formMostrar() {
+		String id = getRequestParameter("paramId");
+		Livro livro = service.obterPeloId(Long.valueOf(id));
+		this.setLivro(livro);
+		return "/vitrine/produto?faces-redirect=true";
+	}
+
+	private String getRequestParameter(String parametro) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		Map<String, String> map = context.getExternalContext().getRequestParameterMap();
+		String id = map.get(parametro);
+		return id;
+	}
+	
+
+	
 
 	public List<Livro> getLivros() {
 		return service.listar();
@@ -38,6 +64,22 @@ public class VitrineBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public ILivroService getService() {
+		return service;
+	}
+
+	public void setService(ILivroService service) {
+		this.service = service;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
 	}
 
 }
